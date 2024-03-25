92fccbace33896cebea7e57e321604ad356bba6e
D Guthrie <dguthrie@posteo.net>
2024-03-25 18:40:54 +0000

    Simplify R script and remove _us variants

--- examples/sentinel_cages/prep.R
+++ examples/sentinel_cages/prep.R
@@ -3,22 +3,16 @@ library (purrr)
 library (stringr)
 
 sentinel_cages_raw = read.csv ("sentinel_cages/Sentinel_cage_sampling_info_update_01122022.csv")
-new_col_names = colnames (sentinel_cages_raw) |> 
-                lapply (\(c) stringr::str_replace_all (c, "\\.", "_"))
+
+#new_col_names = colnames (sentinel_cages_raw) %>%
+#                lapply (\(c) stringr::str_replace_all (c, "\\.", "_"))
 
 # Original column names with dots
-sentinel_cages_cleaned = sentinel_cages_raw |>
-    mutate (across (everything (), ~str_replace (., ',', ';'))) |>
-    mutate_if (is.character, list (~na_if (., "")))
+sentinel_cages_cleaned = sentinel_cages_raw %>%
+    mutate (across (everything (), ~str_replace (., ',', ';'))) %>%
+    mutate_if (is.character, list (~na_if (., ""))) #%>%
+    #set_names (new_col_names)
 
-# Column names with underscores instead of dots
-sentinel_cages_cleaned_us = sentinel_cages_cleaned |>
-    set_names (new_col_names)
-
-sentinel_cages_cleaned |>
+sentinel_cages_cleaned %>%
     write.table ('sentinel_cages/sentinel_cages_cleaned.csv'
                , quote=F, row.names=F, sep=',', na='')
-
-sentinel_cages_cleaned_us |>
-    write.table ('sentinel_cages/sentinel_cages_cleaned_us.csv'
-                 , quote=F, row.names=F, sep=',', na='')
