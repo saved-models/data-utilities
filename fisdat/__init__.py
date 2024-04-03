ba020c27a15c490be903c3fcbceaac73d02b7cbf
D Guthrie <dguthrie@posteo.net>
2024-04-03 13:58:21 +0100

    Set `tag_prefix' properly for `versioneer'

--- fisdat/__init__.py
+++ fisdat/__init__.py
@@ -1,3 +1,6 @@
 from . import _version
 __version__ = _version.get_versions()['version']
 __full_version__ = _version.get_versions()['full-revisionid']
+
+from . import _version
+__version__ = _version.get_versions()['version']
