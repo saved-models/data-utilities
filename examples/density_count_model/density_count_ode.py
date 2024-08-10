adjust model to include copepods falling off and fix bug with probability computation

examples/density_count_model/density_count_ode.py
@@ -5,10 +5,10 @@ import sys
 import json
 import numpy as np
 import datetime
+from itertools import chain
 from scipy.integrate import ode as solve
 from scipy.optimize import minimize, Bounds
 from scipy.stats import wasserstein_distance
-
 
 '''
 See https://github.com/linkml/linkml/issues/1994
@@ -17,21 +17,22 @@ NA_NONE = [" ", "", "#N/A", "#N/A N/A", "#NA", "-1.#IND", "-1.#QNAN"
          , "-NaN", "-nan", "1.#IND", "1.#QNAN", "<NA>", "N/A", "NA"
          , "NULL", "NaN", "None", "n/a", "nan", "null"]
 
-def derivative(k_inf, acc, density, volume, most_observed):
+def derivative(k_inf, k_acc, k_fall, density, volume, dimensionality):
     """
     Return a function implementing the ODE version of the sea lice accumulation
     model. This ODE system generates the histogram as a function of time.
 
-    It requires the rate constant k_inf and the accumulation factor, as well as
+    It requires the rate constants k_inf and k_acc, as well as
     a `density`, a function of time, and the volume of the cage. Number of lice is
     computed as `density(t)*volume`.
     """
-    kn = np.array([k_inf] + [n*k_inf*acc for n in range(1,most_observed+1)])
+    kn = np.array([k_inf] + [k_acc for n in range(1,dimensionality)])
+    kf = np.array([0] + [n*k_fall for n in range(1,dimensionality)])
     def ode(t, F):
         C = density(t) * volume
-        dF0dt = -kn[0]*F[0]*C
-        dFndt = [kn[n-1]*F[n-1]*C - kn[n]*F[n]*C for n in range(1,most_observed)]
-        df50dt = kn[most_observed-1]*F[most_observed-1]*C
+        dF0dt = k_fall*F[1]*C - kn[0]*F[0]*C
+        dFndt = [kn[n-1]*F[n-1]*C + kf[n+1]*F[n+1]*C - kf[n]*F[n]*C - kn[n]*F[n]*C for n in range(1,dimensionality-1)]
+        df50dt = kn[-1]*F[-1]*C - kf[-1]*F[-1]*C
         return np.array([dF0dt] + dFndt + [df50dt])
     return ode
 
@@ -63,7 +64,7 @@ def cagedist(filename, column, max_count):
             raw_counts[total] = raw_counts.get(total, 0) + 1
 
     count_items   = raw_counts.items()
-    total_lice    = sum([k*v for (k,v) in count_items])
+    total_fish    = sum([v for (k,v) in count_items])
     most_observed = max([k for (k,v) in count_items])
     if (max_count < 10 and most_observed > 10): ## otherwise hist() below fails
         nominal_max = most_observed +1
@@ -75,44 +76,40 @@ def cagedist(filename, column, max_count):
     all_counts    = {k: v for (k, v) in zip(range(0, nominal_max), [0]*nominal_max)}
     for (freq, count) in count_items:
         if(freq < nominal_max):
-            prob_count = count / total_lice
+            prob_count = count / total_fish
             probabilities[freq] = prob_count
             all_counts[freq]    = count
     hist = np.array([raw_counts.get(i, 0) for i in range(nominal_max)])
-    hist = downsample(hist, 10)
+    #hist = downsample(hist, 10)
     res = (nominal_max-1, all_counts, probabilities, hist/hist.sum())
     return res
 
-def objective(y0, t0, t1, density, volume, weight, p_measured, most_observed):
+def l2(a, b):
+    return np.sqrt((a-b).dot(a-b))
+
+def objective(y0, t0, t1, density, volume, p_measured, dimensionality):
     """
     Return an objective function to be minimised. Requires the initial
     conditions, `y0`, `t0`, and stopping time, `t1`. Also requires parameters, a
-    density function, volume, relative weight of zero values compared to
-    non-zero values, and measured probability distribution.
+    density function, volume, and measured probability distribution.
     """
     p_measured_zero   = p_measured[0]
     p_measured_renorm = p_measured[1:]/p_measured[1:].sum()
     def f(x):
         if any(c < 0 for c in x):
             return np.inf
-        params = { "k_inf": x[0], "acc": x[1] }
+        params = { "k_inf": x[0], "k_acc": x[1], "k_fall": x[2] }
 
-        ode = derivative(params["k_inf"], params["acc"], density, volume, most_observed)
+        ode = derivative(params["k_inf"], params["k_acc"], params["k_fall"], density, volume, dimensionality)
         solver = solve(ode)
         solver.set_initial_value(y0, t0)
         y = solver.integrate(t1)
 
-        hist = downsample(y, len(p_measured) - 1)
+        p_simulated = np.array([ c if c >= 0 else 0 for c in y ])
+        p_simulated = p_simulated / p_simulated.sum()
 
-        p_simulated = hist/hist.sum()
-        p_simulated_zero = p_simulated[0]
-        p_simulated_renorm = p_simulated[1:]/p_simulated[1:].sum()
-
-        dz = weight*wasserstein_distance([p_measured_zero , 1.0 - p_measured_zero],
-                                         [p_simulated_zero, 1.0 - p_simulated_zero])
-        dc = (1-weight)*wasserstein_distance(p_measured_renorm, p_simulated_renorm)
-
-        return dz + dc
+        return wasserstein_distance(range(len(p_measured)), range(len(p_simulated)),
+                                    p_measured, p_simulated)
     return f
 
 def density(filename, timecol, denscol, fmt=None):
@@ -155,14 +152,16 @@ def density(filename, timecol, denscol, fmt=None):
 def cli():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", default=False, action="store_true", help="Verbose output")
-    parser.add_argument("-l", "--limit", type=int, default=168, help="Simulation time (hours)")
+    parser.add_argument("-l", "--limit", type=float, default=168, help="Simulation time (hours)")
     parser.add_argument("-f", "--format", default=None, help="Time format for density data (see strptime)")
     parser.add_argument("-c", "--csv", default=False, action="store_true", help="CSV output")
     parser.add_argument("-s", "--delimiter", default="\t", help="Delimiter for CSV/TSV output (default TSV)")
-    parser.add_argument("--k_inf", type=float, default=0.002, help="Infection rate")
-    parser.add_argument("--acc", type=float, default=1, help="Increase in infection rate per louse")
-    parser.add_argument("--weight", type=float, default=0.5, help="Relative weight of zeros")
+    parser.add_argument("--k_inf", type=float, default=0.02, help="Infection rate")
+    parser.add_argument("--k_acc", type=float, default=0.02, help="Accumulation rate per louse")
+    parser.add_argument("--k_fall", type=float, default=0.01, help="Detachment rate per louse")
     parser.add_argument("--max-count", type=int, default=-1, help="Maximum count")
+    parser.add_argument("-p", "--plot", help="Output a PNG plot")
+    parser.add_argument("-t", "--title", help="Title for PNG plot")
     parser.add_argument("cagedata", help="Cage data file")
     parser.add_argument("cagecolumn", help="Column in cage data file to use for counts")
     parser.add_argument("densitydata", help="Density data file")
@@ -175,70 +174,111 @@ def cli():
     
     result["obs"]["counts"] = observed_counts
     result["obs"]["probs"]  = observed_probabilities
-    
-    y0 = np.array([100] + [0 for _ in range(1,most_observed+1)])
+
+    dimensionality = 2*most_observed
+    y0 = np.array([100] + [0 for _ in range(1,dimensionality)])
     t0 = 0
     t1 = args.limit
 
-    if args.weight < 0 or args.weight > 1:
-        raise ValueError(f"bad value for weight ({args.weight}), should be between [0,1]")
-    
     density_const = lambda t: 0.1
     volume = 10
 
-    obj = objective(y0, t0, t1, density_const, volume, args.weight, p_measured, most_observed)
-    x0 = [args.k_inf, args.acc]
-    bounds = Bounds([1e-4,0.25], [1e-4,0.25])
-    res = minimize(obj, x0, method='nelder-mead',
+    obj = objective(y0, t0, t1, density_const, volume, p_measured, dimensionality)
+    x0 = [args.k_inf, args.k_acc, args.k_fall]
+    bounds = Bounds([1e-4,10], [1e-4,10], [0,10])
+    res = minimize(obj, x0, method='bfgs',
                    options={'disp': args.verbose})
 
     result["ref"]["k_inf"] = res.x[0]
-    result["ref"]["acc"]   = res.x[1]
+    result["ref"]["k_acc"] = res.x[1]
+    result["ref"]["k_fall"] = res.x[2]
     result["ref"]["dist"]  = res.fun
 
-    ode = derivative(res.x[0], res.x[1], density_const, volume, most_observed)
+    ode = derivative(res.x[0], res.x[1], res.x[2], density_const, volume, dimensionality)
     solver = solve(ode)
     solver.set_initial_value(y0, t0)
     y = solver.integrate(t1)
     result["ref"]["totals"] = { n: c/100 for n, c in enumerate(y) if c > 0 }
     
     density_csv = density(args.densitydata, args.densitytime, args.densitycolumn, args.format)
-    obj = objective(y0, t0, t1, density_csv, volume, args.weight, p_measured, most_observed)
-    x0 = [args.k_inf, args.acc]
-    bounds = Bounds([1e-4,0.25], [1e-4,0.25])
-    res = minimize(obj, x0, method='nelder-mead',
+    obj = objective(y0, t0, t1, density_csv, volume, p_measured, dimensionality)
+    x0 = [args.k_inf, args.k_acc, args.k_fall]
+    bounds = Bounds([1e-4,10], [1e-4,10], [0,10])
+    res = minimize(obj, x0, method='bfgs',
                    options={'disp': args.verbose})
 
     result["test"]["k_inf"] = res.x[0]
-    result["test"]["acc"]   = res.x[1]
+    result["test"]["k_acc"] = res.x[1]
+    result["test"]["k_fall"] = res.x[2]
     result["test"]["dist"]  = res.fun
 
-    ode = derivative(res.x[0], res.x[1], density_csv, volume, most_observed)
+    ode = derivative(res.x[0], res.x[1], res.x[2], density_csv, volume, dimensionality)
     solver = solve(ode)
     solver.set_initial_value(y0, t0)
     y = solver.integrate(t1)
     result["test"]["totals"] = { n: c/100 for n, c in enumerate(y) if c > 0 }
 
     if args.csv:
-        fields = ["count", "ref", "test"]
+        fields = ["count", "obs", "ref", "test"]
         writer = csv.DictWriter(sys.stdout, delimiter=args.delimiter, fieldnames = fields)
         writer.writerow({k:k for k in fields})
         combined = [{ "count": c,
+                      "obs": result["obs"]["probs"][c],
                       "ref": result["ref"]["totals"][c],
                       "test": result["test"]["totals"][c] }
-                    for c in result["ref"]["totals"]]
+                    for c in result["obs"]["probs"]]
         [writer.writerow(row) for row in combined]
 
-        meta = ["series", "k_inf", "acc", "dist"]
+        meta = ["series", "k_inf", "k_acc", "k_fall", "dist"]
         writer = csv.DictWriter(sys.stderr, delimiter=args.delimiter, fieldnames = meta)
         writer.writerow({k:k for k in meta})
         for series in ["ref", "test"]:
             writer.writerow({ "series": series,
                               "k_inf": result[series]["k_inf"],
-                              "acc": result[series]["acc"],
+                              "k_acc": result[series]["k_acc"],
+                              "k_fall": result[series]["k_fall"],
                               "dist": result[series]["dist"] })
     else:
         print(json.dumps(result, indent=4))
+
+    if args.plot is not None:
+        from matplotlib import pyplot as plt
+
+        fig, ax = plt.subplots(1,1, figsize=(8, 5))
+        def bars(series, label):
+            keys = sorted(list(result[series][label].items()), key=lambda x: x[0])
+            x = [n for n, _ in keys][:most_observed]
+            height = [p for _, p in keys][:most_observed]
+            return np.array(x), np.array(height)
+
+        x, height = bars("obs", "probs")
+        ax.bar(x-0.25, height, width=0.2, label="Observed")
+        x, height = bars("ref", "totals")
+        ax.bar(x, height, width=0.2, label="Constant")
+        x, height = bars("test", "totals")
+        ax.bar(x+0.25, height, width=0.2, label="Forced")
+
+        ctext = """\
+          k_inf        k_acc        k_fall        dist
+const  {:0.4f}    {:0.4f}    {:0.4f}     {:.06f}
+forced {:0.4f}    {:0.4f}    {:0.4f}     {:.06f}
+
+{:d} fish sampled
+""".format(result["ref"]["k_inf"], result["ref"]["k_acc"], result["ref"]["k_fall"], result["ref"]["dist"],
+           result["test"]["k_inf"], result["test"]["k_acc"], result["test"]["k_fall"], result["test"]["dist"],
+           sum(result["obs"]["counts"].values()))
+        maxp = max(chain(result["obs"]["probs"].values(),
+                         result["ref"]["totals"].values(),
+                         result["test"]["totals"].values()))
+        ax.text(0.2*most_observed,0.75*maxp, ctext)
+
+        ax.set_xlabel("Count of lice")
+        ax.set_ylabel("Probability of finding on fish")
+        if args.title is not None:
+            ax.set_title(args.title)
+        ax.legend()
+
+        fig.savefig(args.plot)
 
 if __name__ == '__main__':
     cli()